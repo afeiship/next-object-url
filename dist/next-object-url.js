6712c9590da5eb6f4c27288807520935853bb8bf
afeiship <1290657123@qq.com>
2019-05-21 14:42:21 +0800

    add: test

---
+++ dist/next-object-url.js
@@ -0,0 +1,32 @@
+/*!
+ * name: next-object-url
+ * url: https://github.com/afeiship/next-object-url
+ * version: 1.0.0
+ * license: MIT
+ */
+
+(function() {
+  var global = global || this || window || Function('return this')();
+  var nx = global.nx || require('next-js-core2');
+  var GLOBAL_URL = global.URL || global.webkitURL;
+  var createObjectURL = GLOBAL_URL.createObjectURL;
+  var revokeObjectURL = GLOBAL_URL.revokeObjectURL;
+
+  var NxObjectUrl = nx.declare('nx.ObjectUrl', {
+    statics: {
+      create: function(inObject, inOptions) {
+        var url = createObjectURL(inObject, inOptions);
+        return {
+          url: url,
+          destroy: function() {
+            return revokeObjectURL(url);
+          }
+        };
+      }
+    }
+  });
+
+  if (typeof module !== 'undefined' && module.exports) {
+    module.exports = NxObjectUrl;
+  }
+})();
